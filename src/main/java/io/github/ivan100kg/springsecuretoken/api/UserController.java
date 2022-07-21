3f13061a9874f1dfa7271dd9d215efea0e7dfe94
Ivan B <ivar110kg@gmail.com>
2022-07-21 12:41:24 +0400

    Add save User

--- src/main/java/io/github/ivan100kg/springsecuretoken/api/UserController.java
+++ src/main/java/io/github/ivan100kg/springsecuretoken/api/UserController.java
@@ -4,9 +4,7 @@ import io.github.ivan100kg.springsecuretoken.model.AppUser;
 import io.github.ivan100kg.springsecuretoken.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -19,5 +17,10 @@ public class UserController {
     @GetMapping("/users")
     public ResponseEntity<List<AppUser>> getUsers() {
         return ResponseEntity.ok().body(userService.getUsers());
+    }
+
+    @PostMapping("/user/save")
+    public ResponseEntity<AppUser> saveUser(@RequestBody AppUser user) {
+        return ResponseEntity.ok().body(userService.saveUser(user));
     }
 }
