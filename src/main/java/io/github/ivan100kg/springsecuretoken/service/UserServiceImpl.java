941c01c0d95551eae317de721cc196da00d9dcb3
Ivan B <ivar110kg@gmail.com>
2022-07-21 12:36:05 +0400

    Update services

--- src/main/java/io/github/ivan100kg/springsecuretoken/service/UserServiceImpl.java
+++ src/main/java/io/github/ivan100kg/springsecuretoken/service/UserServiceImpl.java
@@ -1,7 +1,7 @@
 package io.github.ivan100kg.springsecuretoken.service;
 
+import io.github.ivan100kg.springsecuretoken.model.AppUser;
 import io.github.ivan100kg.springsecuretoken.model.Role;
-import io.github.ivan100kg.springsecuretoken.model.User;
 import io.github.ivan100kg.springsecuretoken.repo.RoleRepo;
 import io.github.ivan100kg.springsecuretoken.repo.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +9,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 @Service
 public class UserServiceImpl implements UserService{
@@ -17,35 +19,42 @@ public class UserServiceImpl implements UserService{
     @Autowired
     private RoleRepo roleRepo;
 
+    Logger logger = Logger.getAnonymousLogger();
+
     @Override
     @Transactional
-    public User saveUser(User user) {
-        return userRepo.save(user);
+    public AppUser saveUser(AppUser appUser) {
+        logger.log(Level.INFO, "Saving new user {} to the DB");
+        return userRepo.save(appUser);
     }
 
     @Override
     @Transactional
     public Role saveRole(Role role) {
+        logger.log(Level.INFO, "Saving new role {} to the DB");
         return roleRepo.save(role);
     }
 
     @Override
     @Transactional
     public void addRoleToUser(String username, String roleName) {
-        User user = userRepo.findByUsername(username);
+        logger.log(Level.INFO, "Adding new role {} to User {}");
+        AppUser appUser = userRepo.findByUsername(username);
         Role role = roleRepo.findByName(roleName);
-        user.getRoles().add(role);
+        appUser.getRoles().add(role);
     }
 
     @Override
     @Transactional
-    public User getUser(String username) {
+    public AppUser getUser(String username) {
+        logger.log(Level.INFO, "Fetching user");
         return userRepo.findByUsername(username);
     }
 
     @Override
     @Transactional
-    public List<User> getUsers() {
+    public List<AppUser> getUsers() {
+        logger.log(Level.INFO, "Fetching all users");
         return userRepo.findAll();
     }
 }
