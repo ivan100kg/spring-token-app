3d6e9974b4f408ad5d28023ede7f071e27443137
Ivan B <ivar110kg@gmail.com>
2022-07-21 10:46:44 +0400

    Set up .gitignore

---
+++ src/main/java/io/github/ivan100kg/springsecuretoken/SpringSecureTokenApplication.java
@@ -0,0 +1,13 @@
+package io.github.ivan100kg.springsecuretoken;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecureTokenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecureTokenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/ivan100kg/springsecuretoken/SpringSecureTokenApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.ivan100kg.springsecuretoken;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecureTokenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
